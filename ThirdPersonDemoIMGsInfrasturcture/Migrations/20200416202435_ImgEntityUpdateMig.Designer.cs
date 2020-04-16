4a477b304b8fb21a359b0843c62a2d988e898412
AEstradaGrech <alvaro.estrada89+1@gmail.com>
2020-04-16 22:26:24 +0200

    Updating ImageModel, adding ImageMgmtService and ImageDto

--- ThirdPersonDemoIMGsInfrasturcture/Migrations/20200416202435_ImgEntityUpdateMig.Designer.cs
+++ ThirdPersonDemoIMGsInfrasturcture/Migrations/20200416202435_ImgEntityUpdateMig.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ThirdPersonDemoIMGsInfrasturcture.Context;
 
 namespace ThirdPersonDemoIMGsInfrasturcture.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20200416202435_ImgEntityUpdateMig")]
+    partial class ImgEntityUpdateMig
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
